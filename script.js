8da0f4c3dc3550a840a045a858fa1f13efb1c0f0
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2018-03-13 09:49:21 -0600

    CHANGES
    changes

--- script.js
+++ script.js
@@ -18,3 +18,56 @@ window.onload = function() {
    setInterval(changeImage,2750);
 }
 //End of Slide Show
+
+window.onload = function() {
+
+var reviews = [5, 4, 2 ,1, 4, 2, 5, 1, 6, 3];
+
+var c = document.getElementById("graph");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+// first bar
+ctx.lineTo((25*1), (25));
+ctx.stroke;
+ctx.lineTo((25*1),(25));
+ctx.stroke;
+ctx.lineTo((0),(25));
+ctx.stroke;
+ctx.fill();
+// second bar
+ctx.moveTo(0,50);
+ctx.lineTo((25*2), (50));
+ctx.stroke;
+ctx.lineTo((25*2),(50));
+ctx.stroke;
+ctx.lineTo((0),(50));
+ctx.stroke;
+ctx.fill();
+// third bar
+ctx.moveTo(0, 75);
+ctx.lineTo((25*3), 75);
+ctx.stroke;
+ctx.lineTo((25*3),(75));
+ctx.stroke;
+ctx.lineTo((0),(75));
+ctx.stroke;
+ctx.fill();
+// fourth bar
+ctx.moveTo(0, 100);
+ctx.lineTo((25*4), (100));
+ctx.stroke;
+ctx.lineTo((25*4),(100));
+ctx.stroke;
+ctx.lineTo((0),(100));
+ctx.stroke;
+ctx.fill();
+// fourth bar
+ctx.moveTo(0, 125);
+ctx.lineTo((25*4), (125));
+ctx.stroke;
+ctx.lineTo((25*4),(125));
+ctx.stroke;
+ctx.lineTo((0),(125));
+ctx.stroke;
+ctx.fill();
+}
